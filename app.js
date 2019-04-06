7d8d1d47ecf262666d9d6b827aba528e5f9e59a7
Viet <vanviet8367@gmail.com>
2019-04-07 00:57:46 +0700

    Update

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let express = require('express');
-let app = express();
-let port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extented: true}));
+app.use(bodyParser.json());
+
 let router = require('./api/routes');
 let db = require('./configs/db');
 router(app);
+
+app.use((req, res) => {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
 
 app.listen(port);
 
