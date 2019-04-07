118a8cf93fcdfcb1d6529360b2cb22c1f9648426
Viet <vanviet8367@gmail.com>
2019-04-07 13:04:52 +0700

    Delete POST request

--- api/controllers/quote.controller.js
+++ api/controllers/quote.controller.js
@@ -8,18 +8,15 @@ module.exports = {
                 res.status(500).json(err);
         })
     }
-    // ,post: (req, res) => {
-    //     let quote = {
-    //         quote: req.body.quote,
-    //         author: req.body.author
-    //     };
+  //   ,post: (req, res) => {
+  //       let quote = {
+  //           quote: req.body.quote,
+  //           author: req.body.author
+  //       };
 
-    //     let quote = new Quote ({
-    //         quote: 'Mục tiêu bạn đạt được nhiều bao nhiêu không quan trọng bằng tầm nhìn của bạn cao tới đâu.',
-    //         author: 'Jessica Savitch'
-    //     });
-    //     quote.save()
-    //         .then(result => res.status(200).json({msg: 'Success'}))
-    //         .catch(err => res.status(500).json({msg: 'Error:' + err}));
-    // }
+  //       let quote = new Quote (quote);
+  //       quote.save()
+  //           .then(result => res.status(200).json({msg: 'Success'}))
+  //           .catch(err => res.status(500).json({msg: 'Error:' + err}));
+  //   }
 }
