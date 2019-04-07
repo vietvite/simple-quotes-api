268e7ad9f9cc73c1d64c7593bbf393cc86e4d662
Viet <vanviet8367@gmail.com>
2019-04-07 15:00:05 +0700

    Ajust start script

--- api/routes.js
+++ api/routes.js
@@ -3,5 +3,4 @@ module.exports = (app) => {
     let quoteController = require('./controllers/quote.controller');
     app.route('/api/quote')
         .get(quoteController.get)
-        .post(quoteController.post);
 }
