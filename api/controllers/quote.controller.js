18f9c0deaadc87cc72fd3112ecab05d8d63f0f2b
Viet <vanviet8367@gmail.com>
2019-04-08 10:04:34 +0700

    Edit post api

--- api/controllers/quote.controller.js
+++ api/controllers/quote.controller.js
@@ -8,15 +8,22 @@ module.exports = {
                 res.status(500).json(err);
         })
     }
-  //   ,post: (req, res) => {
-  //       let quote = {
-  //           quote: req.body.quote,
-  //           author: req.body.author
-  //       };
+    ,post: (req, res) => {
+        let quotes = req.body.quotes;
+        Quote.collection.insert(quotes, function(err, docs) {
+            if(!err)
+                res.status(200).json({"msg": "Multiple documents inserted to database!"});
+            else
+                res.status(500).json({"msg": err});
 
-  //       let quote = new Quote (quote);
-  //       quote.save()
-  //           .then(result => res.status(200).json({msg: 'Success'}))
-  //           .catch(err => res.status(500).json({msg: 'Error:' + err}));
-  //   }
+        })
+
+        // let quotes = new Quote ([
+        //     {"quote": "Nếu bạn đi đúng đường và luôn luôn sẵn sàng bước tiếp, sớm muộn gì bạn cũng thành đạt được thành công.", "author": "Barack Obama"},
+        //     {"quote": "Nếu bạn muốn lưu lại dấu chân mình trên trảng cát thời gian hay chắc chắn rằng bạn đi đôi giày lao động."}
+        // ]);
+        // quotes.save()
+        //     .then(result => res.status(200).json({msg: 'Success'}))
+        //     .catch(err => res.status(500).json({msg: 'Error:' + err}));
+    }
 }
--- api/routes.js
+++ api/routes.js
@@ -3,4 +3,5 @@ module.exports = (app) => {
     let quoteController = require('./controllers/quote.controller');
     app.route('/api/quote')
         .get(quoteController.get)
+        // .post(quoteController.post);
 }
