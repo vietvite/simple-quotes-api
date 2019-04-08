921987e902811de7f02194c869de9ae7878d6d73
Viet <vanviet8367@gmail.com>
2019-04-08 10:16:19 +0700

    Add / GET request

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,7 @@
 
 module.exports = (app) => {
     let quoteController = require('./controllers/quote.controller');
+    app.route('/').get((req, res) => res.send(`<h1>Quote API</h1>`));
     app.route('/api/quote')
         .get(quoteController.get)
         // .post(quoteController.post);
