a3724eb5c42c1c73a2dea53b6e1aaba055ce4046
Van Viet <vanviet8367@gmail.com>
2019-04-08 10:42:44 +0700

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 require('dotenv').config();
-const db_url = process.env.DB_LOCALHOST || process.env.DB_mLab_URL;
+const db_url = process.env.DB_mLab_URL || process.env.DB_LOCALHOST;
 
 class Database{
     constructor(){
