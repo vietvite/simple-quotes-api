c07179af884fdfea878a1c82389fb4c54eb84a7c
Viet <vanviet8367@gmail.com>
2019-04-08 17:06:35 +0700

    Add configs

---
+++ configs/db.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+require('dotenv').config();
+const db_url = process.env.DB_LOCALHOST || process.env.DB_mLab_URL;
+
+class Database {
+    constructor(){
+        this._connect();
+    }
+    _connect(){
+        mongoose.connect( db_url, {newUserUrlParser: true} );
+            .then(() => console.log("Database connected!"));
+            .catch((err) => console.log("Database connection error: " + err));
+    }
+}
+module.exports = new Database;
