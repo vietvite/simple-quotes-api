664213162eea9e6c2c7251d7cb99c541dc09cfa0
vietvite <vanviet8367@gmail.com>
2019-10-05 01:23:23 +0700

    Fix wrong populate param name

--- common/crud.js
+++ common/crud.js
@@ -2,32 +2,32 @@
 const mongoose = require('mongoose');
 const { callbackRes, errorRes } = require('./response');
 
-function create(model, popudate = []) {
+function create(model, populate = []) {
   return (req, res) => {
     const newData = new model({
       _id: new mongoose.Types.ObjectId(),
       ...req.body,
     });
     newData.save()
-      .then((rs) => rs.popudate(...popudate, callbackRes(res)))
+      .then((rs) => rs.populate(...populate, callbackRes(res)))
       .catch((err) => errorRes(res, err));
   };
 }
 
-function read(model, popudate = []) {
+function read(model, populate = []) {
   return (req, res) => {
-    model.find(...req.body, callbackRes(res)).popudate(...popudate);
+    model.find(...req.body, callbackRes(res)).populate(...populate);
   };
 }
 
-function update(model, popudate = []) {
+function update(model, populate = []) {
   return (req, res) => {
     model.findByIdAndUpdate(
       req.params._id,
       req.body,
       { new: true },
       callbackRes(res),
-    ).popudate(...popudate);
+    ).populate(...populate);
   };
 }
 
