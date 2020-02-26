5528ebe15ab080fcc12fb3bc0a70bf5cc4f0a655
vietvite <vanviet8367@gmail.com>
2020-02-26 12:38:57 +0700

    Remove ignore client/build

---
+++ client/build/precache-manifest.f56866af42445a308c321bcafbe5874a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7856a1351720cea3943a9dadb601eb5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a643ddd4188a639c0d9d",
+    "url": "/static/css/main.d4d9d065.chunk.css"
+  },
+  {
+    "revision": "f546aa90245a36fde704",
+    "url": "/static/js/2.1ab0db74.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1ab0db74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a643ddd4188a639c0d9d",
+    "url": "/static/js/main.b7ecd3ba.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
